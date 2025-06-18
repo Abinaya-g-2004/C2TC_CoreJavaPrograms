bbc237624a8e95bfabf121a5551c5df0eef38503
Abinaya G <abinayagopi0509@gmail.com>
2025-06-18 19:01:18 +0530

    This project contains my core java constructor program

---
+++ C2TC_CoreJavaPrograms/src/day3/Constructor/Customerentity.java
@@ -0,0 +1,18 @@
+package day3.Constructor;
+
+public class Customerentity {
+	public static void main(String[]args) {
+		Customerentity cu=new Customerentity();
+		System.out.print(cu);
+		
+		Customerentity cu1=new Customerentity();
+		System.out.print(cu1);
+		
+		Customerentity cu2=new Customerentity();
+		System.out.print(cu2);
+		
+		
+		
+	}
+
+}
---
+++ C2TC_CoreJavaPrograms/src/day3/Constructor/Customerentitymain.java
@@ -0,0 +1,44 @@
+package day3.Constructor;
+
+public class Customerentitymain {
+	private int id;
+	private String name;
+	private String city;
+	public Customerentitymain(){
+		this.id=101;
+		this.name="Jayashree";
+		this.city="Chennai";
+	}
+	public Customerentitymain(int id,String name) {
+		this.id=103;
+		this.name="Pune";
+	}
+	public Customerentitymain(int id,String name,String city) {
+		this.id=102;
+		this.name="jack";
+		this.city="BAnglore";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Customerentity [id=" + id + ", name=" + name + ", city=" + city + ", getId()=" + getId()
+				+ ", getName()=" + getName() + ", getCity()=" + getCity() +  "]";
+	}
+}	
