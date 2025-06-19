1bdd6ad1d8700073d7f08f189e8e3b5f0e7117e4
Abinaya G <abinayagopi0509@gmail.com>
2025-06-19 11:03:04 +0530

    This project contains my core java singleinheritance program

---
+++ C2TC_CoreJavaPrograms/src/day5/singleheritance/SLInheritDemo.java
@@ -0,0 +1,10 @@
+package day5.singleheritance;
+
+public class SLInheritDemo {
+	public static void main(String[] args) {
+		Trainer t1=new Trainer(101,"unknown","chennai","program Trainer","chennai");
+		System.out.println(t1);
+	}
+	
+
+}
