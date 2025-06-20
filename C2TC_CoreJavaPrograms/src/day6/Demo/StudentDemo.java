a810687959a8e72240c2787736eeb4d1260cc073
Abinaya G <abinayagopi0509@gmail.com>
2025-06-20 11:36:30 +0530

    This project contains my core java static programs

--- C2TC_CoreJavaPrograms/src/day6/Demo/StudentDemo.java
+++ C2TC_CoreJavaPrograms/src/day6/Demo/StudentDemo.java
@@ -1,6 +1,8 @@
 package day6.Demo;
 
 public class StudentDemo {
+	
+	
 	public static void main(String[] args) {
 		Student s=new Student();
 		System.out.println(s);
@@ -12,5 +14,12 @@ public class StudentDemo {
 		System.out.println(s2);
 		
 		}
-
+	public static void main(char c)
+	{
+		System.out.println("hello from character main method..");
+	}
+	public static void main()
+	{
+		System.out.println("hello from no argss main method..");
+	}
 }
