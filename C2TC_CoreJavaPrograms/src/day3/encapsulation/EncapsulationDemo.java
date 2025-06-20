312a2ca2287c3c32dade3a8fd8f36d0b0d204920
Abinaya G <abinayagopi0509@gmail.com>
2025-06-20 19:31:09 +0530

    This project contains my core java encapsulation programs

---
+++ C2TC_CoreJavaPrograms/src/day3/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,12 @@
+package day3.encapsulation;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setId(101);
+		obj.setName("Abi");
+		obj.setAge(20);
+		System.out.println(obj);
+	}
+
+}
