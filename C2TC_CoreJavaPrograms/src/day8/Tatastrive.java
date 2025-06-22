fa9f3de065c68e7ef97b6ff15f92be6da821b539
Abinaya G <abinayagopi0509@gmail.com>
2025-06-22 20:57:17 +0530

    This project contains core java abstract class programs

---
+++ C2TC_CoreJavaPrograms/src/day8/Tatastrive.java
@@ -0,0 +1,10 @@
+package day8;
+
+public class Tatastrive extends Training {
+	public void session() {
+		System.out.println("Java Session is going on...");
+	}
+
+}
+
+
---
+++ C2TC_CoreJavaPrograms/src/day8/TnsCg.java
@@ -0,0 +1,12 @@
+package day8;
+
+public class TnsCg extends Training {
+	public void session ()//abstract method
+	{
+		System.out.println("Java Full Stack session going on....");
+	}
+
+	
+
+}
+
