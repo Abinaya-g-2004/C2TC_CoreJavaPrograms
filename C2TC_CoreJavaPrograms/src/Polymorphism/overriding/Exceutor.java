0d69e7efc8e9e3849612f45cda02660a8a03e60e
Abinaya G <abinayagopi0509@gmail.com>
2025-07-01 11:41:53 +0530

    This project contains core java polymorphism overriding programs

---
+++ C2TC_CoreJavaPrograms/src/Polymorphism/overriding/Exceutor.java
@@ -0,0 +1,18 @@
+package Polymorphism.overriding;
+
+public class Exceutor {
+	public static void main(String[] args) {
+		
+		//dynamic binding or late binding
+		Shape s1=new Shape();
+		
+		s1.calArea();
+		
+		s1=new Square(5.25f);
+		s1.calArea();
+		
+		s1=new Rectangle(5.2d,4.5d);
+		s1.calArea();
+	}
+
+}
---
+++ C2TC_CoreJavaPrograms/src/Polymorphism/overriding/Square.java
@@ -0,0 +1,15 @@
+package Polymorphism.overriding;
+
+public class Square extends Shape {
+	public Square(float area)
+	{
+		super.area=area;
+		
+	}
+	@Override
+	public void calArea() {
+		System.out.println("From square class:"+super.area*super.area);
+		
+	}
+
+}
