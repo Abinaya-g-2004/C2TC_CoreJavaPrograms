fccc107425cdf66242ece58b7fa368a732bc2dba
Abinaya G <abinayagopi0509@gmail.com>
2025-07-07 11:49:41 +0530

    This project contains my core java programs

--- C2TC_CoreJavaPrograms/src/day13/threads/ChildThread.java
+++ C2TC_CoreJavaPrograms/src/day13/threads/ChildThread.java
@@ -1,6 +1,25 @@
 package day13.threads;
 
-public class ChildThread {
+public class ChildThread extends Thread {
+		private int n;
+		private String msg;
+
+		public ChildThread(int n2, String msg2) {
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public void run() {
+			for (int i = 1; i <= n; i++) {
+				try {
+					Thread.sleep(300);
+				} catch (InterruptedException e) {
+					System.err.println("Thread interrupted: " + e.getMessage());
+				}
+				System.out.println(msg + i + " " + Thread.currentThread().getName());
+			}
+		}
+	}
 	
 
-}
+
